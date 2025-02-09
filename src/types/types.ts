4b92209051cb29ea983ce9b54482b34f4c6ca4f1
utharanambiar <utharanambiar@gmail.com>
2025-02-09 15:15:24 +0530

    fix: add types for charts

---
+++ src/types/types.ts
@@ -0,0 +1,35 @@
+export type ComboChartData = {
+  month: string;
+  revenue: number;
+  profit: number;
+};
+
+export type DonutChartData = {
+  category: string;
+  value: number;
+};
+
+type DatasetType =
+  | {
+      type: "bar";
+      label: string;
+      data: number[];
+      backgroundColor: string;
+      borderColor: string;
+      borderWidth: number;
+    }
+  | {
+      type: "line";
+      label: string;
+      data: number[];
+      borderColor: string;
+      backgroundColor: string;
+      fill: boolean;
+      tension: number;
+    };
+
+// Define the type for the chart that supports both 'bar' and 'line' datasets
+export type ComboChart = {
+  labels: string[];
+  datasets: DatasetType[];
+};
