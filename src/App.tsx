4386ad5e57ce2e38b6a2cc3eba336d7a63e22567
utharanambiar <utharanambiar@gmail.com>
2025-02-09 12:33:03 +0530

    chore: initial setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+      <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Hello, Vite + React + TypeScript + Tailwind!
+      </h1>
+    </div>
+  )
+}
+
+export default App
